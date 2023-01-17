5ac82bde9aaa0b279354ce041d81416df336a4b8
Lazarica-Petrut <uzzy.yp@gmail.com>
2023-01-17 09:58:00 +0200

    Third version

--- test_suit_proiect_final.py
+++ test_suit_proiect_final.py
@@ -4,9 +4,10 @@ from HTMLTestRunner.runner import HTMLTestRunner
 from sauce_demo_test import SauceDemoTestCase
 from selenium_site_test import SeleniumEasyTest
 
-
+# Test Suit Class
 class TestingClass(unittest.TestCase):
 
+    # Test Suit for all the other tests
     def test_suit(self):
         my_test_suit = unittest.TestSuite()
         my_test_suit.addTests([
@@ -14,6 +15,7 @@ class TestingClass(unittest.TestCase):
             unittest.defaultTestLoader.loadTestsFromTestCase(SeleniumEasyTest)
         ])
 
+        # Test Runner HTML Report
         my_test_runnner = HTMLTestRunner(
             output = "report",
             title = "Test report",
